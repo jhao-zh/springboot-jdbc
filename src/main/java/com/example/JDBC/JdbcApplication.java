f0ad284a5eeed0168d65fe995b67b02c38d92280
eclipse_jhao <Administrator@USER-20200825FO>
2020-12-20 20:26:34 +0800

    2020/12/20 spring-boot-starter-JDBC 使用注解

--- src/main/java/com/example/JDBC/JdbcApplication.java
+++ src/main/java/com/example/JDBC/JdbcApplication.java
@@ -7,37 +7,54 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
 import com.example.JDBC.po.User;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class,
-		DataSourceTransactionManagerAutoConfiguration.class })
+//@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class,
+//		DataSourceTransactionManagerAutoConfiguration.class })  //多个数据源需配置
+@SpringBootApplication
+/**
+ ** 实现CommandLineRunner 类 ，需重写 run(String... args)方法
+ * */
 public class JdbcApplication implements CommandLineRunner {
 
 	@Autowired
+	/*
+	 * * JdbcTemplate 是Spring对JDBC的封装，目的是使JDBC更加易于使用。 *
+	 * 他运行核心的JDBC工作流，如Statement的建立和执行，而我们只需要提供SQL语句和提取结果。
+	 */
+	// 该程序创建了两个template bean 自动注入会查询到两个，如只创建一个template对象接收会报错
+	//用集合可以同时接收两个bean对象
+	// JdbcTemplate templates; 再该程序中会报错，除非用@primary注释确认以哪个bean为主
 	List<JdbcTemplate> templates;
 
+	// 启动类
 	public static void main(String[] args) {
 		SpringApplication.run(JdbcApplication.class, args);
 	}
 
+	// 查询方法 查询指定template的数据源的数据
 	public void sql(int i) {
 		String sql = "select * from users";
+		//接收查询获得的行数据接收类型，将行数据以User对象的形式存储
 		RowMapper<User> rowMapper = new BeanPropertyRowMapper<User>(User.class);
+		//list集合接收返回的User对象，query()方法：  sql为查询语句，rowMapper为行数据存储类型
 		List<User> users = templates.get(i).query(sql, rowMapper);
 		System.out.println(users.toString());
 	}
 
 	@Override
+	/**
+	 ** run(String... args) 该方法是SpringBoot的启动监听方法，该程序初始化后，启动时会调用该方法 
+	 * */
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("start >>>>>>>>>>>>>>");
-		Random random = new Random(); 
+		//随机调用template对象
+		Random random = new Random();
 		sql(random.nextInt(2));
 	}
 }
--- src/main/java/com/example/JDBC/configuration/JDBCConfiguration.java
+++ src/main/java/com/example/JDBC/configuration/JDBCConfiguration.java
@@ -10,8 +10,10 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import com.mysql.cj.jdbc.MysqlDataSource;
 
 @Configuration
+//数据源配置类
 public class JDBCConfiguration {
 	@Bean
+	//连接test库
 	public DataSource dataSource() throws Throwable {
 		MysqlDataSource dataSource = new MysqlDataSource();
 		dataSource.setUrl("jdbc:mysql://localhost:3306/test?serverTimezone=UTC");
@@ -22,6 +24,7 @@ public class JDBCConfiguration {
 
 	@Bean
 	@Primary
+	//连接test2库并以该数据源为主
 	public DataSource dataSource2() throws Throwable {
 		MysqlDataSource dataSource = new MysqlDataSource();
 		dataSource.setUrl("jdbc:mysql://localhost:3306/test2?serverTimezone=UTC");
@@ -31,11 +34,13 @@ public class JDBCConfiguration {
 	}
 
 	@Bean(name = "jdbctemplate")
+	//创建一个连接test库的jdbctemplate bean
 	public JdbcTemplate jdbcTemplate() throws Throwable {
 		return new JdbcTemplate(dataSource());
 	}
 
 	@Bean(name = "jdbctemplate2")
+	//创建一个连接test2库的jdbctemplate bean
 	public JdbcTemplate jdbcTemplate2() throws Throwable {
 		return new JdbcTemplate(dataSource2());
 	}
